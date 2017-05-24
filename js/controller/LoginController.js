d551e016b03302cdc5358c183ee6ff859796af51
amandapersampa <amandapersampa@hotmail.com>
2017-05-24 12:02:41 -0300

    adição do sair/entrar

--- js/controller/LoginController.js
+++ js/controller/LoginController.js
@@ -3,10 +3,18 @@
          $scope.submit = function() {
              if ($scope.usuario == 'admin' && $scope.senha == 'admin') {
                  $rootScope.loggedIn = true; //set no banco que o usuario esta logado
+                 $scope.entrarSair = "Sair";
                  $location.path("/inicio");
              } else {
                  alert("Usuário ou Senha Inválidos");
              }
 
+         }
+         $scope.logout = function() {
+             if ($rootScope.loggedIn) {
+                 $rootScope.loggedIn = false; //set no banco que o usuario esta logado
+                 $scope.entrarSair = "Entrar";
+                 $location.path("/");
+             }
          }
      }]);
